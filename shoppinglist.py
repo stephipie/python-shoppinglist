8c625c6abf73dacfaaced0e0847733bede5ee363
stephipie <stephi.pietschmann@gmail.com>
2024-10-02 15:10:30 +0200

    add new feature add_item to shoppinglist

--- shoppinglist.py
+++ shoppinglist.py
@@ -2,3 +2,12 @@
 # First we set a variable with the name shoppinglist
 shoppinglist = []
 
+# Add_item
+# First we ask the user for input
+
+def add_item():
+    item = input("Please insert the article you want to add to your shoppinglist: ")
+    print(f"You want to buy {item}") 
+    shoppinglist.append(item)
+
+add_item()
