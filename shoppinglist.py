cf131f4c60dcb2e614fdc32079f992c0394610dd
stephipie <stephi.pietschmann@gmail.com>
2024-10-02 21:11:45 +0200

    add main function

--- shoppinglist.py
+++ shoppinglist.py
@@ -37,5 +37,6 @@ while True:
         break                                                           #while/break loop
     else:
         print("Invalid selection. Please choose 1, 2 or 3.")   
-
+    if __name__ == "__main__":
+        main()
 
