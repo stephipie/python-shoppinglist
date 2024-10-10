bc92285c2e879e45faec23925aa1db5d0517bf98
stephipie <stephi.pietschmann@gmail.com>
2024-10-10 16:45:46 +0200

    add search function

--- shoppinglist.py
+++ shoppinglist.py
@@ -38,6 +38,16 @@ WHERE id = ?
     conn.commit()
     print(f"update groceries with id {id}")  
 
+# create search function
+def search_groceries(name):
+    cursor.execute('''
+SELECT * FROM shoppinglist WHERE name = ?
+''', (name,))
+    shoppinglist = cursor.fetchall()
+    print(f"Looking for groceries {name}: ")
+    for groceries in shoppinglist:
+        print(groceries)
+
 # create delete function
 def delete_groceries(id):
     cursor.execute('''
@@ -46,6 +56,7 @@ DELETE FROM shoppinglist WHERE id = ?
     conn.commit()
     print(f"Groceries with id {id} has been deleted.")
 
+
 # While-Loop to keep the programm running 
 def main():
     while True:
@@ -53,10 +64,11 @@ def main():
         print("1. Add groceries to your shoppinglist")
         print("2. Show shoppinglist")
         print("3. Update groceries")
-        print("4. Delete groceries")
-        print("5. End the programm")
+        print("4. Search for groceries")
+        print("5. Delete groceries")
+        print("6. End the programm")
 
-        choice = int(input("Please choose 1, 2, 3, 4 or 5: "))
+        choice = int(input("Please choose 1, 2, 3, 4, 5 or 6: "))
 
         if choice == 1:
             print("Please enter groceries you want to add to your shoppinglist: ")
@@ -74,14 +86,18 @@ def main():
             price = input("price: ")
             update_groceries(id, name, amount, price)
         elif choice == 4:
+            print("Please enter the name of the groceries you are looking for: ")
+            name = input("name: ")
+            search_groceries(name)
+        elif choice == 5:
             print("Please enter the id of the groceries you want to delete: ")
             id = input("id: ")
             delete_groceries(id)
-        elif choice == 5:
+        elif choice == 6:
             print("Program is ended! Goodbye, see you soon.")
             break                                                           # breaks the loop
         else:
-            print("Invalid input. Please choose 1, 2, 3, 4 or 5.")   
+            print("Invalid input. Please choose 1, 2, 3, 4, 5 or 6.")   
 if __name__ == "__main__":
     main()
 
